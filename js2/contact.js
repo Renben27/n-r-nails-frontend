77fed8a6b7ff49db1f82a193876234f9e01b6784
Renben27 <horvathrenata296@gmail.com>
2025-04-01 21:09:37 +0200

    contact adatbazisfelvetel proba

--- js2/contact.js
+++ js2/contact.js
@@ -1,5 +1,26 @@
 const profileIcon = document.getElementsByClassName('profile-icon')[0];
 const nailsLogo = document.getElementsByClassName('nailslogo')[0];
+const sendbtn = document.getElementById('send');
+
+sendbtn.addEventListener('click', sendMessage);
+async function sendMessage() {
+    const name = document.getElementById('name').value;
+    const phone = document.getElementById('phone').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('uzenet').value;
+    console.log(email, name, phone, uzenet);
+
+    const res = await fetch('/api/contact', {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify({ email, name, telefon, uzenet })
+    });
+    console.log(res);
+    const data = await res.json();
+    document.getElementById('siker').textContent = data.message;
+}
 
 profileIcon.addEventListener('click', () => {
     window.location.href = ('./personaldata.html');
