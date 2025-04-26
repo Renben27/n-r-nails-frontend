e8ba99e27c2e37af4cb98086dde49bf3ebdc2c49
Renben27 <horvathrenata296@gmail.com>
2025-04-26 14:22:41 +0200

    make reviews aboutus-html-css-js and home-html-css-js the same

--- js2/aboutus.js
+++ js2/aboutus.js
@@ -16,18 +16,18 @@ iconDate.addEventListener('click', () => {
 
 
 
+
 const wrapper = document.querySelector(".reviews-wrapper");
 const prevBtn = document.getElementById("prevBtn");
 const nextBtn = document.getElementById("nextBtn");
 
 let scrollAmount = 0;
-const scrollStep = 300; // Egy elem szélessége + gap
+const scrollStep = 260; // Egy elem szélessége + gap
 const autoScrollInterval = 9000; // 3 másodpercenként mozog
 let autoScroll;
 
 // Automatikus görgetés indítása
 function startAutoScroll() {
-    stopAutoScroll();
     autoScroll = setInterval(() => {
         const maxScroll = wrapper.scrollWidth - wrapper.clientWidth;
         if (scrollAmount >= maxScroll) {
@@ -42,7 +42,7 @@ function startAutoScroll() {
 // Automatikus görgetés leállítása (ha a felhasználó kattint)
 function stopAutoScroll() {
     clearInterval(autoScroll);
-    
+    setTimeout(startAutoScroll, 500); //.... múlva újraindul
 }
 
 // Manuális léptetés
@@ -65,3 +65,32 @@ prevBtn.addEventListener("click", () => {
 
 // Indítás oldalbetöltéskor
 startAutoScroll();
+
+
+
+// Vélemények betöltése az API-ról
+async function loadOpinions(offset = 0) {
+    const res = await fetch(`/api/getopinions?offset=${offset}`);
+    if (!res.ok)
+        return;
+
+    const opinions = await res.json();
+    const lista = document.querySelector('.reviews-wrapper');
+    lista.innerHTML = ''; // előző vélemények törlése
+
+    opinions.forEach(opinion => {
+        lista.innerHTML += `
+            <div class="reviews-box">
+                <p class="name">${opinion.nev}</p>
+                <p class="opinions">"${opinion.velemeny}"</p>
+                <p class="date">${new Date(opinion.datum).toLocaleDateString('hu-HU')}</p>
+            </div>
+        `;
+    });
+}
+
+
+// Oldal betöltésekor vélemények betöltése
+document.addEventListener('DOMContentLoaded', () => {
+    loadOpinions();
+});
