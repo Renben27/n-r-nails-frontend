b84cf163aa3da02562dedd4e269dd067351ff3a7
= <=>
2025-04-29 09:01:28 +0200

    calendar.js /uploads proba2+ #ebc6eb

--- js2/calendar.js
+++ js2/calendar.js
@@ -111,10 +111,10 @@ document.addEventListener("DOMContentLoaded", () => {
                         width: 600,
                         padding: '3em',
                         color: '#716add',
-                        background: '#886383', // Tetszés szerint cserélheted
+                        background: '#ebc6eb', // Tetszés szerint cserélheted
                         backdrop: `
                             rgba(0,0,123,0.4)
-                            url("/uploads/nyan-cat.gif")  // Nyan Cat gif a háttérben
+                            url("uploads/nyan-cat.gif")  // Nyan Cat gif a háttérben
                             left top
                             no-repeat
                         `
